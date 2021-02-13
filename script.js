5d3a517063e02aa8e74cbe6849ad851cc6f2d6a8
Jason <jasonpsmith180@gmail.com>
2021-02-13 11:08:47 -0700

    add confirms to gather user inputs

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
-// Create array with lowercase characters for the password
+// Array with lowercase characters for the password
 var lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-// Create array with uppercase characters for the password
+// Array with uppercase characters for the password
 var upperCaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-// Create array with number characters for the password
+// Array with number characters for the password
 var numberCharacters = ["1","2","3","4","5","6","7","8","9","0"];
 
-// Create array with special characters for the password
+// Array with special characters for the password
 var specialCharacters = ["~","!","@","#","$","%","^","&","*","(",")","-","+","{","}","<",">","?"];
 
-// Create a functions to prompt user for password criteria
-function getPasswordCriteria() {
+// Create a function to prompt user for password criteria
+function generatePassword() {
 
-  // Create a variable for password length and set the value to the user input
-  var length = window.prompt("How many characters would you like the password to be (MIN: 8 MAX: 128)");
+  // Variable for password length set to the value of the user input
+  var length = parseInt(prompt("How many characters would you like the password to be (MIN: 8 MAX: 128)"));
 
-  // // Create conditional if statement to see if the user put a number less than 8 and prompt them to enter a larger number
-  // if (length < 8) {
-  //   alert
-  // }
-  // // Create conditional if statement to see if the user put a number more than 128 and prompt them to enter a smaller number
-  // if (length > 128){
-  //   alert
-  // }
+  // Create conditional statement to see if the user input a put a number less than 8 or larger than 128 and prompt them to enter a number between 8 and 128
+  if (length < 8 || length > 128) {
+    length = parseInt(prompt("Please enter a number between 8 and 128"));
+  }
+  
+  // Prompts to have users choose password criteria
+  // Create prompt to include lowercase characters
+  var useLowerCaseCharacters = confirm("Do you want the password to use lowercase characters?");
 
-  // // Create prompts to have users choose password criteria
-  // // Create prompt to include lowercase characters
-  // var useLowerCaseCharacters
+  // Create prompt to include uppercase characters
+  var useUpperCaseCharacters = confirm("Do you want the password to use uppercase characters?");
 
-  // // Create prompt to include uppercase characters
-  // var useUpperCaseCharacters
+  // Create prompt to include number characters
+  var useNumberCharacters = confirm("Do you want the password to use number characters?");
 
-  // // Create prompt to include number characters
-  // var useNumberCharacters
+  // Create prompt to include special characters
+  var useSpecialCharacters = confirm("Do you want the password to use special characters?");
 
-  // // Create prompt to include special characters
-  // var useSpecialCharacters
-
-  // // Create conditional statement to check that at least one criteria has been chosen
-  // // if (useLowerCaseCharacters === false && useUpperCaseCharacters === false && useNumberCharacters === false && useSpecialCharacters === false) {
-  // //   alert
-  // // }
+  // Create conditional statement to check that at least one criteria has been chosen
+  if (useLowerCaseCharacters === false && useUpperCaseCharacters === false && useNumberCharacters === false && useSpecialCharacters === false) {
+    alert("You must choose at least one criteria. Please try again.");
+    generatePassword();
+  }
 
   // // Create an object to store all of the user's choices
   // var passwordCriteria
+
+  console.log(length);
+  console.log(useLowerCaseCharacters);
+  console.log(useUpperCaseCharacters);
+  console.log(useNumberCharacters);
+  console.log(useSpecialCharacters);
 }
-console.log(length);
+
 
 // Create a function to pull a random index from an array
 function getRandom(params) {
